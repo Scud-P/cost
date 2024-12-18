770323626b74fe343fcb1f1dec1a43e051de108b
PScud <escudie.pierre@gmail.com>
2024-12-18 10:37:16 -0500

    First commit

---
+++ src/main/kotlin/com/coutuapi/cost/CostApplication.kt
@@ -0,0 +1,11 @@
+package com.coutuapi.cost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CostApplication
+
+fun main(args: Array<String>) {
+	runApplication<CostApplication>(*args)
+}
